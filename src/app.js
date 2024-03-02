726f26ef1dfa55a43b7c3756cffdd63e30d11853
Bula <klara23.03.1900@gmail.com>
2024-03-02 23:51:47 +0500

    view, routing and state

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-'use strict'
+import { MainView } from './views/main/main.js';
+
+class App {
+	routes = [
+		{ path: "", view: MainView }
+	];
+	appState = {
+		favorites: []
+	};
+
+	constructor() {
+		//  обработчик события hashchange окна браузера,
+		// который будет вызывать метод routes объекта App при изменении хэша в URL
+		window.addEventListener('hashchange', this.route.bind(this));
+		// Метод bind(this) используется, чтобы привязать контекст this к методу routes.
+		this.route();
+	}
+
+	route() {
+		if (this.currentView) {
+			this.currentView.destroy();
+		}
+		const view = this.routes.find(r => r.path == location.hash).view;
+		this.currentView = new view(this.appState);
+		this.currentView.render();
+	}
+}
+
+new App();
